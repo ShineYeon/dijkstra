a7c42aea77a9fe9164e5698e0d007bebc6e98992
heeyeon <chlgmldus2@naver.com>
2021-05-27 23:10:00 +0900

    Initial Commit

---
+++ dijkstra/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  dijkstra
+//
+//  Created by 최희연 on 2021/05/27.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
